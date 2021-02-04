9db2fc6c533eabc677b5d4bb07dc854567f0e5d7
KalaxBDJ <santiago.salazar9208@unaula.edu.co>
2021-02-03 23:07:43 -0500

    Trying to deploy

--- src/app/components/gapiexample/gapiexample.component.ts
+++ src/app/components/gapiexample/gapiexample.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import {}
+import {SocialLoginModule , SocialAuthServiceConfig , GoogleLoginProvider} from 'angularx-social-login';
 
 @Component({
   selector: 'gapiexample',
